5195159f36f43fa788c6b90289bae76bbce11e2e
JD-2006 <85276310+JD-2006@users.noreply.github.com>
2024-01-28 13:39:34 -0800

    Update wifi-reconnect.sh

--- Extras/wifi-reconnect.sh
+++ Extras/wifi-reconnect.sh
@@ -1,13 +1,21 @@
-#!/bin/bash 
- 
-SSID=$(/sbin/iwgetid --raw) 
+#!/bin/bash
 
-if [ -z "$SSID" ] 
-then 
+# Check if WiFi interface is already up
+if ifconfig wlan1 | grep -q "UP"; then
+    SSID=$(/sbin/iwgetid --raw)
+    if [ -z "$SSID" ]; then
+        echo "`date -Is` WiFi interface is already up, but not connected to a network. Trying to reconnect." >> /home/pi/wifi-log.txt
+        sudo ifdown wlan1
+        sleep 20
+        sudo ifup wlan1
+    else
+        echo "`date -Is` WiFi interface is already up and connected to SSID: $SSID. No action needed." >> /home/pi/wifi-log.txt
+    fi
+else
     echo "`date -Is` WiFi interface is down, trying to reconnect" >> /home/pi/wifi-log.txt
     sudo ifdown wlan1
     sleep 10
     sudo ifup wlan1
-fi 
+fi
 
 echo 'WiFi check finished'
